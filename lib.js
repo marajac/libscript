33171488cac6f44ac03cd152a904c14849781f17
marajac <marcusj829@gmail.com>
2017-08-14 16:19:56 -0500

    refactored and cleaned up code

--- lib.js
+++ lib.js
@@ -1,24 +1,30 @@
 // ==UserScript==
 //@include http://libanswers.utsa.edu/record.php*
 
-document.getElementById('qpreselect').addEventListener('click', choiceListener);
+var qbox = document.getElementById('qpreselect');
+var qboxValue = qbox.value;
+var f1 = document.getElementById('f1').value;
+var f2 = document.getElementById('f2').value;
+var f3 = document.getElementById('f3').value;
+var f4 = document.getElementById('f4').value;
+
+qbox.addEventListener('click', choiceListener);
 
 function choiceListener() {
     //laptop/GIFM/Dibs/other tech
-    if(document.getElementById('qpreselect').value == 26292 || document.getElementById('qpreselect').value == 26293
-    	|| document.getElementById('qpreselect').value == 26294 || document.getElementById('qpreselect').value == 26295)
+    if(qboxValue == 26292 || qboxValue == 26293 || qboxValue == 26294 || qboxValue == 26295)
     {
-    	document.getElementById('f1').value = 2;
-    	document.getElementById('f2').value = 1;
-    	document.getElementById('f3').value = 1;
-    	document.getElementById('f4').value = 1;
+    	f1 = 2;
+    	f2 = 1;
+    	f3 = 1;
+    	f4 = 1;
     }
     //course reserve
-    if(document.getElementById('qpreselect').value == 26291)
+    if(qboxValue == 26291)
     {
-    	document.getElementById('f1').value = 1;
-    	document.getElementById('f2').value = 1;
-    	document.getElementById('f3').value = 1;
-    	document.getElementById('f4').value = 1;
+    	f1 = 1;
+    	f2 = 1;
+    	f3 = 1;
+    	f4 = 1;
     }
 }
