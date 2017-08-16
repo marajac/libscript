2c9fefc57aaf649a91151a39404089703bbd5ab2
marajac <marcusj829@gmail.com>
2017-08-16 09:42:23 -0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -3,19 +3,19 @@
 
 var qbox = document.getElementById('qpreselect');
 var qboxValue = qbox.value;
-var f1 = document.getElementById('f1');
-var f2 = document.getElementById('f2').value;
-var f3 = document.getElementById('f3').value;
-var f4 = document.getElementById('f4').value;
 
 qbox.addEventListener('click', choiceListener);
 
 function choiceListener() {
+    var f1 = document.getElementById('f1').value;
+    var f2 = document.getElementById('f2').value;
+    var f3 = document.getElementById('f3').value;
+    var f4 = document.getElementById('f4').value;
     //laptop/GIFM/Dibs/other tech
     if(qboxValue == 27251 || qboxValue == 27252 || qboxValue == 27249 || qboxValue == 27250 || 
       qboxValue == 27253)
     {
-    	f1.value = 2;
+    	f1 = 1;
     	f2 = 1;
     	f3 = 1;
     	f4 = 1;
