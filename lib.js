0cf0b68630ac75f0af6dc1a0c8a9c058c1bf2748
marajac <marcusj829@gmail.com>
2017-08-16 10:50:45 -0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -2,30 +2,28 @@
 //@include http://libanswers.utsa.edu/record.php*
 
 var qbox = document.getElementById('qpreselect');
-var qboxValue = qbox.value;
-var f1 = document.getElementById('f1').value;
-var f2 = document.getElementById('f2').value;
-var f3 = document.getElementById('f3').value;
-var f4 = document.getElementById('f4').value;
+var qboxValue;
 
 qbox.addEventListener('click', choiceListener);
 
 function choiceListener() {
+    qboxValue = qbox.value;
     //laptop/GIFM/Dibs/other tech
     if(qboxValue == 27251 || qboxValue == 27252 || qboxValue == 27249 || qboxValue == 27250 || 
       qboxValue == 27253)
     {
-    	f1 = 2;
-    	f2 = 1;
-    	f3 = 1;
-    	f4 = 1;
+    	setFields(2, 1, 1, 1);
     }
     //course reserve
     if(qboxValue == 27248)
     {
-    	f1 = 1;
-    	f2 = 1;
-    	f3 = 1;
-    	f4 = 1;
+    	setFields(1, 1, 1, 1);
     }
+}
+
+function setFields(f1, f2, f3, f4) {
+    document.getElementById('f1').value = f1;
+    document.getElementById('f2').value = f2;
+    document.getElementById('f3').value = f3;
+    document.getElementById('f4').value = f4;
 }
