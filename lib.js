5f91f9c91508a2e09b37c471700fec35e061a81e
marajac <marcusj829@gmail.com>
2017-08-18 07:15:20 -0500

    Added completed code

--- lib.js
+++ lib.js
@@ -1,8 +1,21 @@
 // ==UserScript==
 //@include http://libanswers.utsa.edu/record.php*
 
+//TODO: Refactor for cleanliness
+
 var qbox = document.getElementById('qpreselect');
-var qboxValue;
+var qboxValue, text, client, qcombo, option, textByLine;
+
+
+client = new XMLHttpRequest();
+client.open('GET', 'https://marajac.github.io/libscript/options.txt');
+client.onreadystatechange = function() {
+    if (client.readyState == 4 && client.status == 200){
+        text = client.responseText.split('\n');
+        createOptions(text);
+    }
+}
+client.send();
 
 qbox.addEventListener('click', choiceListener);
 
@@ -15,9 +28,53 @@ function choiceListener() {
     	setFields(2, 1, 1, 1);
     }
     //course reserve
-    if(qboxValue == 27248)
+    else if(qboxValue == 27248)
     {
     	setFields(1, 1, 1, 1);
+    }
+    //First Floor
+    //Directional
+    else if(qboxValue == 2)
+    {
+        document.getElementById('q').value = "Directional";
+    	setFields(2, 1, 2, 1);
+        swapFields();
+    }
+    //Second Floor
+    //Directional
+    else if(qboxValue == 4)
+    {
+        document.getElementById('q').value = "Directional";
+    	setFields(2, 1, 1, 1);
+        swapFields();
+    }
+    //Stacks Transaction
+    else if(qboxValue == 5)
+    {
+        document.getElementById('q').value = "Stacks Transaction";
+    	setFields(2, 1, 1, 1);
+        swapFields();
+    }
+    //Difficult Reserve
+    if(qboxValue == 6)
+    {
+        document.getElementById('q').value = "Difficult Reserve";
+    	setFields(1, 1, 1, 2);
+        swapFields();
+    }
+    //Phone Directional
+    if(qboxValue == 7)
+    {
+        document.getElementById('q').value = "Phone Directional";
+    	setFields(2, 2, 1, 1);
+        swapFields();
+    }
+    //Phone Reserve
+    if(qboxValue == 8)
+    {
+        document.getElementById('q').value = "Phone Reserve";
+    	setFields(1, 2, 1, 1);
+        swapFields();
     }
 }
 
@@ -26,4 +83,20 @@ function setFields(f1, f2, f3, f4) {
     document.getElementById('f2').value = f2;
     document.getElementById('f3').value = f3;
     document.getElementById('f4').value = f4;
+}
+
+//generate new options
+function createOptions(textByLine){  
+    qcombo = document.getElementById('qpreselect');
+    for(i=0; i < textByLine.length-1; i++){
+        option = document.createElement('option');
+        option.text = textByLine[i];
+        option.value = i+1;
+        qcombo.appendChild(option);
+    }
+}
+
+function swapFields(){
+    showPreDef();
+    document.getElementById('qpreselect').value = 0;
 }
